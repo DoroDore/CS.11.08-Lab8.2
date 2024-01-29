b5493e4b05545206be1232b69d0cc3fd087ff46d
DoroDore <tlyz82@student.cis.eduhk>
2024-01-29 14:26:05 +0800

    SPEEDRUNNING

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private final String mTitle;
+    private final String mArtist;
+    private final ArrayList<String> mListeners = new ArrayList<>();
+    public Song(String title, String artist) {
+        mTitle = title;
+        mArtist = artist;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+    public String getArtist() {
+        return mArtist;
+    }
+    public int howMany(ArrayList<String> listeners) {
+        int newListeners = 0;
+        for (String listener : listeners) {
+            if (!mListeners.contains(listener.toLowerCase())) {
+                mListeners.add(listener.toLowerCase());
+                newListeners++;
+            }
+
+        }
+        return newListeners;
+    }
 }
